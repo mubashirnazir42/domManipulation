0d6ec7e83ddb651d5b30e7d5eb0cf8386743d947
mubashirnazir42 <153961579+mubashirnazir42@users.noreply.github.com>
2024-01-05 16:00:26 +0500

    first dom

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 "use strict";
+
+document.querySelector("h1").textContent = "welcome to DOM manipulation";
+document.querySelector("h1").style.textAlign = "center";
+document.querySelector("h1").style.backgroundColor = "aqua";
+
+document.querySelector("#circle").innerHTML = "hi";
+document.querySelector("#circle").style.color = "blue";
