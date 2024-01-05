88f7895f3b6aa5b320f94cd4ebf4e90a4e206cd8
mubashirnazir42 <153961579+mubashirnazir42@users.noreply.github.com>
2024-01-05 15:05:08 +0500

    domo

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var a = document.querySelector("h1");
+a.innerHTML = "change hogia ustaad";
+
+let yourName = (document.getElementById("your-name").textContent = "hi");
+console.log(yourName);
