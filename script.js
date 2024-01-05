9554c0ad664b6ff12eebfbceb782e6a678644c51
mubashirnazir42 <153961579+mubashirnazir42@users.noreply.github.com>
2024-01-05 19:16:17 +0500

    bulb

--- script.js
+++ script.js
@@ -4,5 +4,36 @@ document.querySelector("h1").textContent = "welcome to DOM manipulation";
 document.querySelector("h1").style.textAlign = "center";
 document.querySelector("h1").style.backgroundColor = "aqua";
 
-document.querySelector("#circle").innerHTML = "hi";
-document.querySelector("#circle").style.color = "blue";
+document.querySelector(".para").textContent =
+  "my name is mubashir, and im learning dom minupluation( not a part of js) its web apis written in javascript thats why we can use it in js!";
+document.querySelector(".para").style.backgroundColor = "yellow";
+document.querySelector(".para").style.textAlign = "center";
+
+let bulb = document.querySelector("#circle");
+let btn = document.querySelector(".btn");
+let val = 0;
+
+btn.addEventListener("click", function () {
+  if (val === 0) {
+    bulb.style.backgroundColor = "yellow";
+    btn.innerHTML = "OFF";
+    val = 1;
+  } else if (val == 1) {
+    bulb.style.backgroundColor = "blue";
+    btn.innerHTML = "ON";
+    val = 2;
+  } else {
+    bulb.style.backgroundColor = "transparent";
+    btn.innerHTML = "blue";
+    val = 0;
+  }
+});
+
+// if (val === 0) {
+//   console.log("hi");
+//   btn.addEventListener("click", function () {
+//     bulb.querySelector.backgroundColor = "transparent";
+//     btn.innerHTML = "OFF";
+//     val = 1;
+//   });
+// }
