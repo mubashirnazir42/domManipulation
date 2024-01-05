8b22dda71379afa400d4b82dcf3373adc84b00b3
mubashirnazir42 <153961579+mubashirnazir42@users.noreply.github.com>
2024-01-05 23:30:33 +0500

    committed

--- script.js
+++ script.js
@@ -1,39 +1,36 @@
 "use strict";
+var btn = document.querySelector(".changed");
+var circle = document.querySelector(".circle");
+var square = document.querySelector(".square");
+let val = Math.trunc(Math.random() * 4);
+console.log(val);
 
-document.querySelector("h1").textContent = "welcome to DOM manipulation";
-document.querySelector("h1").style.textAlign = "center";
-document.querySelector("h1").style.backgroundColor = "aqua";
-
-document.querySelector(".para").textContent =
-  "my name is mubashir, and im learning dom minupluation( not a part of js) its web apis written in javascript thats why we can use it in js!";
-document.querySelector(".para").style.backgroundColor = "yellow";
-document.querySelector(".para").style.textAlign = "center";
-
-let bulb = document.querySelector("#circle");
-let btn = document.querySelector(".btn");
-let val = 0;
-
+console.log();
 btn.addEventListener("click", function () {
   if (val === 0) {
-    bulb.style.backgroundColor = "yellow";
-    btn.innerHTML = "OFF";
-    val = 1;
-  } else if (val == 1) {
-    bulb.style.backgroundColor = "blue";
-    btn.innerHTML = "ON";
-    val = 2;
-  } else {
-    bulb.style.backgroundColor = "transparent";
-    btn.innerHTML = "blue";
-    val = 0;
+    circle.style.backgroundColor = "blue";
+    square.style.backgroundColor = "blue";
+    console.log(val);
+    val = Math.trunc(Math.random() * 4);
+  } else if (val === 1) {
+    circle.style.backgroundColor = "transparent";
+    square.style.backgroundColor = "transparent";
+    console.log(val);
+    val = Math.trunc(Math.random() * 4);
+  } else if (val === 2) {
+    circle.style.backgroundColor = "red";
+    square.style.backgroundColor = "red";
+    console.log(val);
+    val = Math.trunc(Math.random() * 4);
+  } else if (val === 3) {
+    circle.style.backgroundColor = "yellow";
+    square.style.backgroundColor = "yellow";
+    console.log(val);
+    val = Math.trunc(Math.random() * 4);
+  } else if (val === 4) {
+    circle.style.backgroundColor = "green";
+    square.style.backgroundColor = "green";
+    console.log(val);
+    val = Math.trunc(Math.random() * 4) + 1;
   }
 });
-
-// if (val === 0) {
-//   console.log("hi");
-//   btn.addEventListener("click", function () {
-//     bulb.querySelector.backgroundColor = "transparent";
-//     btn.innerHTML = "OFF";
-//     val = 1;
-//   });
-// }
