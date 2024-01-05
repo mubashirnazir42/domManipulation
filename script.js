d25ad8e109ab435c492ebaae72f49451fb15c9d7
mubashirnazir42 <153961579+mubashirnazir42@users.noreply.github.com>
2024-01-05 15:43:26 +0500

    done

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
 "use strict";
-
-var a = document.querySelector("h1");
-a.innerHTML = "change hogia ustaad";
-
-let yourName = (document.getElementById("your-name").textContent = "hi");
-console.log(yourName);
